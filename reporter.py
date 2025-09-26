47abb15bb3c679834b607b6da3ea9b29c3079d44
stnn00 <nguyens6@csp.edu>
2025-09-26 02:26:35 -0500

    Add module docstrings

---
+++ reporter.py
@@ -0,0 +1,7 @@
+"""
+reporter.py
+
+A command-line application that uses live data to report
+information about a city using OpenWeatherMap API,
+writes the data to a CSV file called city_data.csv
+"""
