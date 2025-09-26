c9f6428afdf003022b810a35f4b0b25ae8c39bfb
stnn00 <nguyens6@csp.edu>
2025-09-26 02:50:54 -0500

    Refactor code for PEP8 compliance: spacing and wording

--- reporter.py
+++ reporter.py
@@ -5,6 +5,7 @@ A command-line application that uses live data to report
 information about a city using OpenWeatherMap API,
 writes the data to a CSV file called city_data.csv
 """
+
 # Standard library
 import os # Retrieve API key from system environment
 
@@ -15,14 +16,13 @@ from dotenv import load_dotenv # Access API key from .env file
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
-
 def input_city():
     """Prompt user to enter a city name until non-empty city name is provided."""
     while True:
-        city = input("Enter a City name: ").strip() # Remove leading/trailing whitespaces
+        city = input("Enter a city name: ").strip() # Remove leading/trailing whitespaces
         if city:
             return city
-        print("City name cannot be empty. Please enter a non-empty city name.")
+        print("Please enter a valid city name.")
 
 
 if __name__ == "__main__":
