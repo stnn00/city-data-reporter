e219b861bd601b296d66076fcf16e0df360d047b
stnn00 <nguyens6@csp.edu>
2025-09-26 02:35:17 -0500

    Add initial imports and load .env for API key

--- reporter.py
+++ reporter.py
@@ -5,3 +5,12 @@ A command-line application that uses live data to report
 information about a city using OpenWeatherMap API,
 writes the data to a CSV file called city_data.csv
 """
+# Standard library
+import os # Retrieve API key from system environment
+
+# Third-party libraries
+from dotenv import load_dotenv # Access API key from .env file
+
+# Load .env file and get API key
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
